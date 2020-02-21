3b2fb34e1809de7d2f7b4eeda2cf891d0fdf8c11
Harrison Levin <harrison483@gmail.com>
2020-02-21 11:03:46 -0700

    facade update

--- app/models/search_compiler.rb
+++ app/models/search_compiler.rb
@@ -4,20 +4,20 @@ class SearchCompiler
   end
 
   def title
-    require "pry"; binding.pry
-    @book_data
+    @book_data['docs'][0]['title']
   end
 
   def author
-    @book_data
+    @book_data['docs'][0]['author_name'][0]
   end
 
   def genre
-    @book_data
+    genre = @book_data['docs'][0]['subject']
+    genre == nil ? "No Genre" : genre
   end
 
   def find_reviews
     data = NytBookApi.find_reviews(@book_data['docs'][0]['isbn'][1])
-
+    data['results']
   end
 end
